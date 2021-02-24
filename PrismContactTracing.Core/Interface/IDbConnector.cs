12ce6a55dc0c13f20ab2387a8f9b0fae4ed7e778
kimaurellano <kim.aurellano@yahoo.com>
2021-02-24 12:39:35 -0800

    Added Db Class files

---
+++ PrismContactTracing.Core/Interface/IDbConnector.cs
@@ -0,0 +1,9 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace PrismContactTracing.Core.Interface {
+    public interface IDbConnector {
+        public MySqlConnection DbConnectionInstance { get; set; }
+        public bool Connect();
+        public void Disconnect();
+    }
+}
