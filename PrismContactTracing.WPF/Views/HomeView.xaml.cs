6707494dbf812b3a97d77fbcffbba5130c9a0896
kimaurellano <kim.aurellano@yahoo.com>
2021-03-21 17:55:30 -0700

    Updated UI design

---
+++ PrismContactTracing.WPF/Views/HomeView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace PrismContactTracing.WPF.Views {
+    /// <summary>
+    /// Interaction logic for HomeView.xaml
+    /// </summary>
+    public partial class HomeView : UserControl {
+        public HomeView() {
+            InitializeComponent();
+        }
+    }
+}
