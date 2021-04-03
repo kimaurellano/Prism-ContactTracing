2936d8f93c6d39648bb0b4e449d67880c8f23c07
kimaurellano <kim.aurellano@yahoo.com>
2021-04-02 23:01:55 -0700

    Added InsertQuery.cs

---
+++ PrismContactTracing.Core/DataComponent/InsertQuery.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Data.Sqlite;
+using PrismContactTracing.Core.Interface;
+using System.Data;
+
+namespace PrismContactTracing.Core.DataComponent {
+    public class InsertQuery : IQueryStrategy {
+
+        private DataTable _dataTable;
+        private DbConnector _dbConnector;
+
+        public string Query { get; set; }
+
+        public InsertQuery() {
+            _dbConnector = new DbConnector();
+        }
+
+        public DataTable DoQuery() {
+            _dbConnector.DbConnectionInstance.Open();
+
+            DataSet _ds = new DataSet("MainDataset");
+            _dataTable = new DataTable();
+            _dataTable = _ds.Tables.Add("MainTable");
+
+            SqliteCommand command = new SqliteCommand(Query + ";", _dbConnector.DbConnectionInstance);
+            command.ExecuteNonQuery();
+            command.Dispose();
+
+            _dbConnector.Disconnect();
+
+            return _dataTable;
+        }
+    }
+}
