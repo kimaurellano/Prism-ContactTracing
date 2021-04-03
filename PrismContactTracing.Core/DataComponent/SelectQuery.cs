cbcb99f0664afc8d7b8812f44a81835c229f457e
kimaurellano <kim.aurellano@yahoo.com>
2021-04-02 23:02:19 -0700

    Refactor SelectQuery.cs

--- PrismContactTracing.Core/DataComponent/SelectQuery.cs
+++ PrismContactTracing.Core/DataComponent/SelectQuery.cs
@@ -17,32 +17,40 @@ namespace PrismContactTracing.Core.DataComponent {
         // Check account existence
         // Get all users
         public DataTable DoQuery() {
-            _dbConnector.DbConnectionInstance.Open();
+            try {
+                _dbConnector.DbConnectionInstance.Open();
 
-            DataSet _ds = new DataSet("MainDataset");
-            _dataTable = new DataTable();
-            _dataTable = _ds.Tables.Add("MainTable");
+                DataSet _ds = new DataSet("MainDataset");
+                _dataTable = new DataTable();
+                _dataTable = _ds.Tables.Add("MainTable");
 
-            SqliteCommand command = new SqliteCommand(Query + ";", _dbConnector.DbConnectionInstance);
-            using (var reader = command.ExecuteReader()) {
-                int columCount = reader.FieldCount;
-                for (int i = 0; i < columCount; i++) {
-                    _dataTable.Columns.Add(reader.GetName(i));
-                }
-
-                while (reader.Read()) {
-                    var currentRow = _dataTable.NewRow();
+                SqliteCommand command = new SqliteCommand(Query + ";", _dbConnector.DbConnectionInstance);
+                using (var reader = command.ExecuteReader()) {
+                    int columCount = reader.FieldCount;
                     for (int i = 0; i < columCount; i++) {
-                        currentRow[i] = reader.GetString(i);
+                        _dataTable.Columns.Add(reader.GetName(i));
                     }
 
-                    _dataTable.Rows.Add(currentRow);
+                    while (reader.Read()) {
+                        var currentRow = _dataTable.NewRow();
+                        for (int i = 0; i < columCount; i++) {
+                            if (reader.IsDBNull(i)) {
+                                currentRow[i] = string.Empty;
+                                continue;
+                            }
+                            currentRow[i] = reader.GetString(i);
+                        }
+
+                        _dataTable.Rows.Add(currentRow);
+                    }
                 }
+
+                _dbConnector.Disconnect();
+
+                return _dataTable;
+            } catch (System.Exception) {
+                throw;
             }
-
-            _dbConnector.Disconnect();
-
-            return _dataTable;
         }
     }
 }
