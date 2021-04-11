7b969492ef853ee330ad26df0fbd2d246b62c385
kimaurellano <kim.aurellano@yahoo.com>
2021-04-11 15:14:13 -0700

    Refactor

---
+++ PrismContactTracing.Core/DataComponent/CheckForMarks.cs
@@ -0,0 +1,64 @@
+﻿using MySql.Data.MySqlClient;
+using PrismContactTracing.Core.Interface;
+using System.Collections.Generic;
+using System.Data;
+
+namespace PrismContactTracing.Core.DataComponent {
+
+    /// <summary>
+    /// This is for checking DataTable with "Mark for" columns.
+    /// Just do something on the data with 'Marked = true'.
+    /// This is made for that purpose obviously duh
+    /// </summary>
+    public class CheckForMarks : IQueryStrategy {
+
+        private DbConnector _dbConnector;
+
+        public string ParameterName { get; set; }
+        public string Procedure { get; set; }
+        public List<string> ParameterValues { get; set; }
+        /// <summary>
+        /// DataTable SHOULD HAVE "Marked for" column
+        /// </summary>
+        public DataTable TargetDataTable { get; set; }
+
+        public CheckForMarks() {
+            _dbConnector = new DbConnector();
+        }
+
+        public DataTable DoQuery() {
+            _dbConnector.Connect();
+
+            
+            List<string> keys = new List<string>();
+            if(TargetDataTable != null) {
+                // When target table is supplied for checking
+                for (int i = 0; i < TargetDataTable.Rows.Count; i++) {
+                    // Rows marked for delete
+                    if ((bool)TargetDataTable.Rows[i].ItemArray[TargetDataTable.Columns.Count - 1]) {
+                        keys.Add(TargetDataTable.Rows[i][TargetDataTable.Columns[0].ColumnName].ToString());
+                    }
+                }
+            } else {
+                // Values are manually supplied and will change accordingly
+                foreach (var value in ParameterValues) {
+                    keys.Add(value);
+                }
+            }
+
+            // Do query for how many rows are changed   
+            foreach (string key in keys) {
+                MySqlCommand command = new MySqlCommand(Procedure, _dbConnector.DbConnectionInstance) {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                command.Parameters.AddWithValue(ParameterName, key);
+
+                command.ExecuteNonQuery();
+                command.Dispose();
+            }
+
+            return null;
+        }
+    }
+}
---
+++ PrismContactTracing.Core/DataComponent/GetDataQuery.cs
@@ -0,0 +1,77 @@
+﻿using MySql.Data.MySqlClient;
+using PrismContactTracing.Core.Interface;
+using System.Collections.Generic;
+using System.Data;
+
+namespace PrismContactTracing.Core.DataComponent {
+    public class GetDataQuery : IQueryStrategy {
+
+        private DataTable _dataTable;
+        private DbConnector _dbConnector;
+
+        public string Procedure { get; set; }
+
+        public List<KeyValuePair<string, string>> Parameters { get; set; }
+
+        public GetDataQuery() {
+            _dbConnector = new DbConnector();
+        }
+
+        // Check account existence
+        // Get all users
+        public DataTable DoQuery() {
+            _dbConnector.Connect();
+
+            DataSet dataset = new DataSet("MainDataset");
+            _dataTable = new DataTable();
+            _dataTable = dataset.Tables.Add("MainTable");
+
+            MySqlCommand command = new MySqlCommand(Procedure, _dbConnector.DbConnectionInstance) { 
+                CommandType = CommandType.StoredProcedure
+            };
+
+            if (Parameters?.Count > 0) {
+                foreach (var parameter in Parameters) {
+                    command.Parameters.AddWithValue(parameter.Key, parameter.Value);
+                }
+            }
+            
+            using (MySqlDataReader reader = command.ExecuteReader()) {
+                if (!reader.HasRows) {
+                    return null;
+                }
+
+                int columCount = reader.FieldCount;
+                for (int i = 0; i < columCount; i++) {
+                    _dataTable.Columns.Add(reader.GetName(i));
+                }
+
+                if (Procedure == "GetResident" || Procedure == "GetResidentsList") {
+                    _dataTable.Columns.Add("Mark for archive", typeof(bool));
+                    _dataTable.Columns["Mark for archive"].DefaultValue = false;
+                } else if (Procedure.Contains("GetArchive")){
+                    _dataTable.Columns.Add("Mark for restore", typeof(bool));
+                    _dataTable.Columns["Mark for restore"].DefaultValue = false;
+                }
+
+                while (reader.Read()) {
+                    var currentRow = _dataTable.NewRow();
+                    for (int i = 0; i < columCount; i++) {
+                        if (reader.IsDBNull(i)) {
+                            currentRow[i] = string.Empty;
+                            continue;
+                        }
+
+                        currentRow[i] = reader.GetString(i);
+                    }
+
+                    _dataTable.Rows.Add(currentRow);
+                }
+            }
+
+            _dbConnector.Disconnect();
+
+            return _dataTable;
+        }
+    }
+}
