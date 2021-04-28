3759f36729ee3bb4558712126c98b34eda107ce2
kimaurellano <kim.aurellano@yahoo.com>
2021-04-28 23:02:40 +0800

    Added settings

---
+++ PrismContactTracing.Core/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using PrismContactTracing.Core.DataComponent;
+using PrismContactTracing.Core.Listener;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace PrismContactTracing.Core.ViewModels {
+    public class SettingsViewModel : BindableBase {
+        public SettingsViewModel() {
+
+        }
+    }
+}
